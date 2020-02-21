c9250989c402994dbf4aa2d769caf7dfb0fc8f6b
GAWoodburn <58796796+GAWoodburn@users.noreply.github.com>
2020-02-21 03:51:24 -0600

    Update HtmlTest.cpp
    I've updated the file with the insert function working with the corresponding strings (a, b, c). I've uploaded a screenshot of the html file to see what the output looks like as well.

--- HtmlTest.cpp
+++ HtmlTest.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 #include <fstream>
-#include <algorithm>
 #include <string>
-#include <iomanip>
 using namespace std;
 
-/* The program, so far, will read in the text and change the desired characters... 
-BUT it won't stream the 'temp' string to the output file 'resulthtml.html' if the strings are changed...
-The commented out lines are not working properly.
-I will continue working on it - Gabriel Woodburn...February 20th*/
+/* The program, so far, will read in the text, change the desired characters
+and insert the corresponding string... 
+I will continue working on it - Gabriel Woodburn...February 21st*/
 
 int main()
 {
 	string temp;
+	string a = "</title>"; 
+	string b = " </comment>"; 
+	string c = " </header>";
 	ifstream file1;
 	ofstream fout;
 
-	file1.open("testhtml.txt");		// Open the test file
+	file1.open("testhtml.txt");	// Open the test file
 	if (file1.fail())
 	{
 		cout << "\nError opening .txt file";
@@ -32,41 +32,43 @@ int main()
 		}
 	}
 
-	fstream input("testhtml.txt");  // Get input from test file
+	fstream input("testhtml.txt", ios::in);  // Get input from test file
 	if (file1) {
 		file1.seekg(0, ios::end);  // Seek until the end of the file
 		size_t len = file1.tellg();
 		file1.seekg(0);
 		string temp(len + 1, '\0');
 		file1.read(&temp[0], len); // Reads the data into 'temp' string
-		file1.close();			   // Close the input text file
-		cout << temp;			   // Used for testing purposes...shows temp info
+		file1.close();		   // Close the input text file
+		cout << temp << endl;	   // Used for testing purposes...shows temp info
 
 		size_t found = temp.find("*T");  // Searching in temp string for '*T'
-		if (found!=string::npos)		 // If the size_t doesn't point to a null position
+		if (found!=string::npos)         // If the size_t doesn't point to a null position
 		{
-			temp.replace(0, 2, "\n<title>");  // Replaces *T with <title>...the 0 indicates the character position
-		}					  // the following 2 indicates the number of characters to replace, i.e "*T"
-
+			temp.replace(0, 3, "<title>");  // Replaces *T with <title>...the 0 indicates the character position, the following 2 indicates the number of characters to replace, i.e "*T"
+			temp.insert(17, a);		// Insert string 'a' at character position 131 within the temp string
+		}
+		
 		size_t findnew = temp.find("*C"); // Searching in temp string for '*C'
-		if (found!=string::npos)		  // If the size_t doesn't point to a null position
+		if (found!=string::npos)	  // If the size_t doesn't point to a null position
 		{
-			temp.replace(21, 2, "<comment>"); // Replaces *C with <comment>...the 21 indicates the character position
-		}					  // the following 2 indicates the number of characters to replace, i.e "*C"
-
-		size_t findagain = temp.find("*H");  // Searching in temp string for '*T'
+			temp.replace(27, 2, "<comment>"); // Replaces *C with <comment>...the 27 indicates the character position, the following 2 indicates the number of characters to replace, i.e "*C"
+			temp.insert(95, b);	  	  // Insert string 'b' at character position 95 within the temp string
+		}
+		
+		size_t findagain = temp.find("*H");  // Searching in temp string for '*H'
 		if (findagain != string::npos)       // If the size_t doesn't point to a null position
 		{
-			temp.replace(90, 2, "<Header>");  // Replaces *H with <Header>...the 90 indicates the character position
-		}					  // the following 2 indicates the number of characters to replace, i.e "*H"
+			temp.replace(107, 2, "<header>");  // Replaces *H with <Header>...the 107 indicates the character position, the following 2 indicates the number of characters to replace, i.e "*H"
+			temp.insert(131, c);		   // Insert string 'c' at character position 131 within the temp string
+		}
 		
-		cout << endl << temp;				  // Display the change 'temp' string
+		cout << endl << temp;			   // Display the change 'temp' string
+		fout << temp << endl;			   // Stream the 'temp' string into the html output file
 	}
-	file1.close();						  // Close the input text file
-	fout << temp << endl;					  // Stream the 'temp' string into the html output file
+	
 	cout << "\n\nThe new file has been saved within resulthtml.html!\n";
-	//file1.close();
-	fout.close();							  // Close the output html file
+	fout.close();					   // Close the output html file
 
 	return 0;
 }
